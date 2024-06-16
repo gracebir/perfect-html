f7f793ca32fce1cb6f3941c56a2aa6714761e2af
gracebir <grace.devolop@gmail.com>
2024-06-16 12:06:21 +0200

    call component to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@
 
 import Header from "./components/Header";
 import TabBar from "./components/TabBar";
+import BottomFilters from "./sections/BottomFilters";
+import TableSection from "./sections/TableSection";
+import TopFilters from "./sections/TopFilters";
 
 function App() {
     return (
@@ -9,8 +12,15 @@ function App() {
             <Header />
             <main className='max-w-7xl mx-auto mt-4 flex flex-col gap-6 lg:gap-9 px-6 md:px-4 lg:px-0'>
                 <TabBar />
-                {/* <div className='w-full flex flex-col gap-4'>
+                <div className='w-full flex flex-col gap-4'>
                     {/* table filter top */}
+                    <TopFilters />
+                    {/* bottom filter */}
+                    <div className='flex flex-col gap-2'>
+                        <BottomFilters />
+                        <TableSection />
+                    </div>
+                </div>
             </main>
         </div>
     );
