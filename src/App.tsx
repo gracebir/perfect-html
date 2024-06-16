4cd0acb3c60d7e40b8dfa06f8bdf4a5f13b82b05
gracebir <grace.devolop@gmail.com>
2024-06-16 12:02:38 +0200

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 /** @format */
 
+import Header from "./components/Header";
+import TabBar from "./components/TabBar";
+
 function App() {
-    return <></>;
+    return (
+        <div className='w-full bg-gray-100 min-h-screen'>
+            <Header />
+            <main className='max-w-7xl mx-auto mt-4 flex flex-col gap-6 lg:gap-9 px-6 md:px-4 lg:px-0'>
+                <TabBar />
+                {/* <div className='w-full flex flex-col gap-4'>
+                    {/* table filter top */}
+            </main>
+        </div>
+    );
 }
 
 export default App;
